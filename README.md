1957f1138709aea6e08a02c3a72f39062a478bae
zoonai <izomboy.id@gmail.com>
2024-08-05 17:01:08 +0700

    added files

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # nesa-bot
 
-1. Membuat Virtual ENV
-# python -m venv myenv
-2. Mengaktifkan Virtual ENV
-+ Mac OS
-	# source myenv/bin/activate
-+ Windows
-	# myenv\Scripts\activate
-+ Linux/Ubuntu
-	# python3 --version
-	# sudo apt update
-	# sudo apt install python3 python3-venv python3-pip
-	# python3 -m venv myenv
-	# source myenv/bin/activate
-3. Install discord.py-self
-# pip install discord.py-self
-4. Ubah YOUR_USER_TOKEN_HERE pada send_message.py
-5. Jalan Bot dengan perintah 
-# python send_message.py
+# 1. Membuat Virtual ENV
+- python -m venv myenv
+# 2. Mengaktifkan Virtual ENV
+1. Mac OS
+	- source myenv/bin/activate
+2. Windows
+	- myenv\Scripts\activate
+3. Linux/Ubuntu
+	- python3 --version
+	- sudo apt update
+	- sudo apt install python3 python3-venv python3-pip
+	- python3 -m venv myenv
+	- source myenv/bin/activate
+# 3. Install discord.py-self
+- pip install discord.py-self
+# 4. Ubah YOUR_USER_TOKEN_HERE pada send_message.py
+# 5. Jalan Bot dengan perintah 
+- python send_message.py
 
--- send_message.py
+++ send_message.py
@@ -1,22 +1,43 @@
 import discord
 from discord.ext import tasks
+import asyncio
 
-TOKEN = 'YOUR_USER_TOKEN_HERE'  # Ganti dengan token akun pengguna Anda
-CHANNEL_ID = 123456789012345678  # Ganti dengan ID channel yang diinginkan
+ACCOUNTS = [
+    {
+        'token': 'YOUR_USER_TOKEN_HERE', # Ganti dengan token akun pengguna Anda
+        'channel_id': 123456789012345678 # Ganti dengan ID channel yang diinginkan
+    }
+    # Tambahkan akun lain di sini
+]
 
-client = discord.Client()
+class MyClient(discord.Client):
+    def __init__(self, token, channel_id):
+        super().__init__()
+        self.token = token
+        self.channel_id = channel_id
 
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}!')
-    send_message.start()
+    async def on_ready(self):
+        print(f'Logged Discord as {self.user}!')
+        self.send_message.start()
 
-@tasks.loop(hours=1, minutes=1)
-async def send_message():
-    channel = client.get_channel(CHANNEL_ID)
-    if channel:
-        await channel.send('!work')
-    else:
-        print('Channel not found')
+    @tasks.loop(hours=1, minutes=2)
+    async def send_message(self):
+        channel = self.get_channel(self.channel_id)
+        if channel:
+            await channel.send('!work')
+        else:
+            print('Channel not found')
 
-client.run(TOKEN)
+    def run_bot(self):
+        self.run(self.token)
+
+clients = []
+
+for account in ACCOUNTS:
+    client = MyClient(account['token'], account['channel_id'])
+    clients.append(client)
+
+async def main():
+    await asyncio.gather(*(client.start(client.token) for client in clients))
+
+asyncio.run(main())
